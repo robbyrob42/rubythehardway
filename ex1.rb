ababe21decf950de60cbf14c3fd1509d64344a45
Rob Jacques <robert.jacques1@gmail.com>
2015-01-06 19:22:13 -0800

    keeping up the good fight

---
+++ ex1.rb
@@ -0,0 +1,6 @@
+puts "Hello World"
+puts "hello again"
+puts "This is fun"
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
---
+++ ex2.rb
@@ -0,0 +1,7 @@
+#thi is a commentohelle 
+
+puts "you shitface " #and your mom too
+
+#this will get boring fast
+
+puts "fascinatingly enough the tensile strenth of a 1/4 in. carriage bolt is 34 ft/lbs"
