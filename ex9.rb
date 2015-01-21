b3713f26b79f2871c59e8c0a06fc00bf3eda217e
Rob Jacques <robert.jacques1@gmail.com>
2015-01-20 22:07:06 -0800

    I'm getting it this time

---
+++ ex9.rb
@@ -0,0 +1,13 @@
+days = "Mon Tue Wed Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+puts "here are the days: #{days}"
+puts "here are the months: #{months}"
+
+puts """
+Theres something goin on here.
+With the three double quotes.
+We'll be able to type as much as we like
+a bunch more lines
+e 
+"""
