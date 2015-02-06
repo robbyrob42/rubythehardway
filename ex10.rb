c74b3e07c5a3f8cacbce8965dfbea4ea28281a24
Rob Jacques <robert.jacques1@gmail.com>
2015-02-05 21:01:12 -0800

    hell yeah gets.chomp, you old sailor you

--- ex10.rb
+++ ex10.rb
@@ -1,11 +1,15 @@
 tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
+help="""sdfsdf
+sdfsdfsdfsdsd
+#{tabby_cat}
+"""
 
 fat_cat = """ 
 I'll do alist:
 \t* Cat Food
-\t* Fishies
+\t* Fishies\x03
 \t* Catnip\n\t* Grass
 """
 
@@ -13,4 +17,5 @@ puts tabby_cat
 puts persian_cat
 puts backslash_cat
 puts fat_cat
+puts help
 
---
+++ ex11.rb
@@ -0,0 +1,10 @@
+print "How old are you?"
+age = gets.chomp.to_i
+print "how tall are you?"
+height = gets.chomp
+print "How much do you weigh?"
+weight = gets.chomp.to_i
+
+sum = age + weight
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy? gross. \n Your fatage is #{sum}"
