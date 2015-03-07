98f4239aa95c2a7fc8567a383a64b901a2312ab8
Rob Jacques <robert.jacques1@gmail.com>
2015-03-07 08:48:28 -0800

    working with fractions and gets

--- ex11.rb
+++ ex11.rb
@@ -1,7 +1,7 @@
 print "How old are you?"
 age = gets.chomp.to_i
 print "how tall are you?"
-height = gets.chomp
+height = gets.chomp 
 print "How much do you weigh?"
 weight = gets.chomp.to_i
 
--- ex12.rb
+++ ex12.rb
@@ -1,12 +1,12 @@
 print "give me a number:"
-number = gest.chomp.to_i
+number = gets.chomp.to_i
 
 bigger = number *100
 puts "A bigger number #{bigger}."
 
 print "give me another number:"
 another = gets.chomp
-number = another.to_i
+number = another.to_f
 
 smaller = number/100
 puts "a smaller number is #{smaller}."
